f0645e47865662202c150f33074dfe4fea343ea5
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2024-07-11 20:25:58 -0300

    add api consumer

--- camel-microservices-a/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_a/routes/c/ActiveMqSenderRouter.java
+++ camel-microservices-a/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_a/routes/c/ActiveMqSenderRouter.java
@@ -3,15 +3,23 @@ package com.github.lucasgmoreira.microservices.camel_microservices_a.routes.c;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:active-mq-timer?period=10000")
-                .transform()
-                .constant("My message for activemq")
-                .log("${body}")
-                .to("activemq:my-activemq-queue");
+//        from("timer:active-mq-timer?period=10000")
+//                .transform()
+//                .constant("My message for activemq")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+
+//        from("file:files/json")
+//                .log("${body}")
+//                .to("activemq:my-activemq-queue");
+        System.out.println("hgere");
+        from("file:files/xml")
+                .log("here ${body}")
+                .to("activemq:my-activemq-xml-queue");
 
     }
 }
--- camel-microservices-b/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_b/routes/ActiveMqReceiver.java
+++ camel-microservices-b/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_b/routes/ActiveMqReceiver.java
@@ -1,13 +1,61 @@
 package com.github.lucasgmoreira.microservices.camel_microservices_b.routes;
 
+import com.github.lucasgmoreira.microservices.camel_microservices_b.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.math.BigDecimal;
+
 public class ActiveMqReceiver extends RouteBuilder {
+
+    @Autowired
+    private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
+
+    @Autowired
+    private MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
+
     @Override
     public void configure() throws Exception {
-        from("activemq:my-activemq-queue")
+//        from("activemq:my-activemq-queue")
+//                .unmarshal()
+//                .json(JsonLibrary.Jackson, CurrencyExchange.class)
+//                .bean(myCurrencyExchangeProcessor)
+//                .bean(myCurrencyExchangeTransformer)
+//                .to("log:receiver-message-from-active-mq");
+
+
+
+        from("activemq:my-activemq-xml-queue")
+                .unmarshal()
+                .jacksonXml(CurrencyExchange.class)
                 .to("log:receiver-message-from-active-mq");
+    }
+}
+
+@Component
+class MyCurrencyExchangeProcessor {
+    Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+
+    public void processMessage(CurrencyExchange currencyExchange) {
+        logger.info("Do some processing with currencyExchange.getConversionMultiple wich is {}", currencyExchange.getConversionMultiple());
+    }
+}
+
+@Component
+class MyCurrencyExchangeTransformer {
+    Logger logger = LoggerFactory.getLogger(MyCurrencyExchangeProcessor.class);
+
+    public CurrencyExchange transformMessage(CurrencyExchange currencyExchange) {
+        currencyExchange.setConversionMultiple(
+                currencyExchange
+                        .getConversionMultiple()
+                        .multiply(BigDecimal.TEN)
+                );
+
+        return currencyExchange;
     }
 }
