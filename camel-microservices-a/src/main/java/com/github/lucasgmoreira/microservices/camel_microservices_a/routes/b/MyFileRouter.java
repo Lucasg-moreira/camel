f8c560cbf4039ada638d4a9ead0c8ff2c5cc16aa
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2024-07-11 21:42:49 -0300

    conditions with simple lang and beans deciders

--- camel-microservices-a/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_a/routes/b/MyFileRouter.java
+++ camel-microservices-a/src/main/java/com/github/lucasgmoreira/microservices/camel_microservices_a/routes/b/MyFileRouter.java
@@ -1,16 +1,49 @@
 package com.github.lucasgmoreira.microservices.camel_microservices_a.routes.b;
 
+import org.apache.camel.Body;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 
-// @Component
+
+@Component
 public class MyFileRouter extends RouteBuilder {
+
+    @Autowired
+    private DeciderBean deciderBean;
 
     @Override
     public void configure() throws Exception {
         from("file:files/input")
-                .log("${body}")
+                .routeId("Files-Input-Route")
+                .transform()
+                .body(String.class)
+                .choice()
+                    .when(simple(("${file:ext} ends with 'xml'")))
+                        .log("The file ends with xml")
+//                    .when(simple("${body} contains 'USD'"))
+                        .when(method(deciderBean))
+                        .log("Isnt a xml file but contains USd")
+                    .otherwise()
+                        .log("Isnt a xml file!")
+                .end()
+                .log("${messageHistory} ${headers}")
                 .to("file:files/output");
+    }
+}
+
+@Component
+class DeciderBean {
+
+    Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+
+    public boolean isThisConditionMet (@Body String body, @Headers Map<String, String> headers) {
+        logger.info("DeciderBean: {}, {}", body, headers);
+        return true;
     }
 }
